100c98c8751da21c8a421b4d35d4a9a00695f378
LiChengLong <6025242704@qq.com>
2018-04-07 16:09:28 +0800

    重新写的方法

--- src/main/java/com/woman/tool/UpdataName.java
+++ src/main/java/com/woman/tool/UpdataName.java
@@ -6,6 +6,10 @@ public class UpdataName {
 	//玉先的
 	//public static  String appid =  "wxbe3edd9434a14587";
   //  public static  String secret =  "47b0814befd1ee161bdb32f462c65b71";
+	//我们公司注册
+//	public static  String appid = 	"wxc40b6179d3eb2ba4";
+//	public static  String secret =  "947f146a015cb8653e55ef2ad26987b1";
+//	
 	public static  String appid = 	"wxc40b6179d3eb2ba4";
 	public static  String secret =  "947f146a015cb8653e55ef2ad26987b1";
 //	https://www.chuanshoucs.com
